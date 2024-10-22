fb90cde0467dbe7c84b6f99a5d35df193a39240c
Rukkhadevata123 <3230102179@zju.edu.cn>
2024-10-23 00:16:42 +0800

    clang

--- clang.nix
+++ clang.nix
@@ -1,8 +1,14 @@
 with import <nixpkgs> {};
-stdenv.mkDerivation {
-  name = "clang-nix-shell";
-  buildInputs = [ pkgs.clang ];
+let
+  gccForLibs = stdenv.cc.cc;
+in stdenv.mkDerivation {
+  name = "llvm-env";
+  buildInputs = [
+    clang
+  ];
+  
   CPLUS_INCLUDE_PATH="${gccForLibs}/include/c++/${gccForLibs.version}:${gccForLibs}/include/c++/${gccForLibs.version}/x86_64-unknown-linux-gnu:${gccForLibs}/lib/gcc/x86_64-unknown-linux-gnu/${gccForLibs.version}/include:${stdenv.cc.libc.dev}/include";
+  
   shellHook = ''
     
   '';
